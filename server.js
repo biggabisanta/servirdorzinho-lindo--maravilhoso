e16395ee528099c1cb4e66cfa71f77b497d368d4
biggabisanta <gaah.dedelly@gmail.com>
2019-05-29 22:18:35 -0300

    adicionar consts, access-control e GET

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const http = require('http')
 
+const comidas = {
+    pratosFavoritos: 
+    [{nome: "Batata frita",
+descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+imagem: "img/Batata-frita.jpg"
+},
+{
+nome: "Macarronada",
+descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+imagem: "img/macarronada.jpg"
+},
+{
+nome: "Falafel",
+descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+imagem: "img/falafel.jpg"
+},
+{
+nome: "Creme de abóbora",
+descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+imagem: "img/creme-de-abobora.jpg"}]}
+
 const servidor = http
     .createServer(function (request, response) { // create.Server é uma função que funciona atraves do http, ela recebe requisições e da resposta
         if (request.url === '/') {
@@ -10,18 +31,21 @@ const servidor = http
 
             if (request.method === 'GET') {
                 response.writeHead(200, {
-                    "Content-Type": "text/html;charset=utf-8"
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
                 })
+                response.write(JSON.stringify(comidas))
             }
-            response.end("<h1> resposta do GET bem lindão </h1")
+            response.end()
         } else if (request.method === 'POST') {
             response.writeHead(201, {
                 "Content-Type": "text/html;charset=utf-8"
             })
             response.end("<h1> Resposta diferentona do post</h1>")
-            
+
         }
     })
 servidor.listen(3000) //portas altas acima de 3000
 
 console.log('Servidor rodando na porta 3000')
+
