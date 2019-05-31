d640344233fc34cd9d5369283615a714d6fab729
biggabisanta <gaah.dedelly@gmail.com>
2019-05-30 21:50:44 -0300

    instal cors, criei o controller, criei novos arquivos

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+const controller = require("./ComidasController")
 
-const comidas = {
-    pratosFavoritos:[
-    ]}
-    const servidor= express()
 
-    servidor.get("/comidas", (resquest, response) => {
-response.send("EITAA PORRA FUNCIONOU!!!")
-    })
+const servidor = express()
+servidor.use(cors())
+
+servidor.get("/comidas", (resquest, response) => {
+  response.send(controller.getAll())
+
+})
 
 servidor.listen(3000) //portas altas acima de 3000
 console.log('Servidor rodando na porta 3000')
